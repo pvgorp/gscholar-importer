bbbe795c6371183c8ab837e3da957d7ffed97f88
pvgorp <git@pietervangorp.com>
2023-04-29 09:41:16 +0200

    Working on inlining

--- crawl.py
+++ crawl.py
@@ -7,10 +7,26 @@ import requests
 from random import randint
 from time import sleep
 
+# this function for the getting inforamtion of the web page
+def get_author_info(author_url):
+
+  #download the page
+  response=requests.get(author_url,headers=headers)
+
+  # check successful response
+  if response.status_code != 200:
+    print('Status code:', response.status_code)
+    raise Exception('Failed to fetch web page ')
+
+  #parse using beautiful soup
+  paper_doc = BeautifulSoup(response.text,'html.parser')
+  return paper_doc
+
 # This function caches scholarly data to out.csv (e.g., citations, URL), for further procesing by custom code
 def inToOut():
-    outFieldnames = ['First name', 'Last name', 'affiliation', 'citations', 'scholar_page','url_pic']
-    with open('out.csv', 'w', newline='') as outCsvfile:
+    #outFieldnames = ['First name', 'Last name', 'affiliation', 'citations', 'scholar_page','url_pic']
+    outFieldnames = ['First name', 'Last name', 'affiliation', 'citations', 'scholar_page','url_pic','citationsAll','citations5Y','hIndexAll','hIndex5Y','keywords']
+    with open('out2.csv', 'w', newline='') as outCsvfile:
         outWriter = csv.DictWriter(outCsvfile, fieldnames=outFieldnames)   
         outWriter.writeheader()
         with open('in.csv', newline='') as csvfile:
@@ -31,60 +47,60 @@ def inToOut():
                     citations= scholEntry['citedby']
                     url_pic= scholEntry['url_picture']
                     scholarPage= "https://scholar.google.com/citations?user="+scholEntry['scholar_id']
+                    doc= get_author_info(scholarPage)
+                    print("Analyzing "+firstName+" "+lastName+" deeper than scholarly...");
+
+                    citationsAll= doc.find_all("td", class_="gsc_rsb_std")[0].text
+                    citations5Y= doc.find_all("td", class_="gsc_rsb_std")[1].text
+                    hIndexAll= doc.find_all("td", class_="gsc_rsb_std")[2].text
+                    hIndex5Y= doc.find_all("td", class_="gsc_rsb_std")[3].text
+                    keywords= doc.find_all("a", class_="gsc_prf_inta gs_ibl")
+                    kw=""
+                    for a in doc.find_all("a", class_="gsc_prf_inta gs_ibl"):
+                        kw=kw+a.text+";"
+                    print("keywords: "+kw)
+        
+                    outWriter.writerow({'First name': firstName, 'Last name': lastName, 'affiliation': affiliation, 'citations': citations, 'scholar_page': scholarPage, 'url_pic': url_pic,
+                        'citationsAll':citationsAll,'citations5Y':citations5Y,'hIndexAll':hIndexAll,'hIndex5Y':hIndex5Y,'keywords':kw})
+                    sleep(randint(1,3))
                     #print("%s %s, %s" % (firstName, lastName, affiliation))
-                    outWriter.writerow({'First name': firstName, 'Last name': lastName, 'affiliation': affiliation, 'citations': citations, 'scholar_page': scholarPage, 'url_pic': url_pic})
+                    #outWriter.writerow({'First name': firstName, 'Last name': lastName, 'affiliation': affiliation, 'citations': citations, 'scholar_page': scholarPage, 'url_pic': url_pic})
          
-headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
-
-# this function for the getting inforamtion of the web page
-def get_author_info(author_url):
-
-  #download the page
-  response=requests.get(author_url,headers=headers)
-
-  # check successful response
-  if response.status_code != 200:
-    print('Status code:', response.status_code)
-    raise Exception('Failed to fetch web page ')
-
-  #parse using beautiful soup
-  paper_doc = BeautifulSoup(response.text,'html.parser')
-  return paper_doc
-
+headers = {'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36'}
 
 #### MAIN HERE
 
 inToOut()
-with open('out.csv', newline='') as csvfile:
-        csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
-        outFieldnames = ['First name', 'Last name', 'affiliation', 'citations', 'scholar_page','url_pic','citationsAll','citations5Y','hIndexAll','hIndex5Y','keywords']
-        with open('out-h-index.csv', 'w', newline='') as outCsvfile:
-            outWriter = csv.DictWriter(outCsvfile, fieldnames=outFieldnames)   
-            outWriter.writeheader()
+# with open('out.csv', newline='') as csvfile:
+#         csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+#         outFieldnames = ['First name', 'Last name', 'affiliation', 'citations', 'scholar_page','url_pic','citationsAll','citations5Y','hIndexAll','hIndex5Y','keywords']
+#         with open('out-h-index.csv', 'w', newline='') as outCsvfile:
+#             outWriter = csv.DictWriter(outCsvfile, fieldnames=outFieldnames)   
+#             outWriter.writeheader()
 
-            for row in csvreader:
-                print(row)
-                firstName= row['First name']
-                lastName= row['Last name']
-                citations= row['citations']
-                scholarPage= row['scholar_page']
-                affiliation= row['affiliation']
-                url_pic= row['url_pic']
+#             for row in csvreader:
+#                 print(row)
+#                 firstName= row['First name']
+#                 lastName= row['Last name']
+#                 citations= row['citations']
+#                 scholarPage= row['scholar_page']
+#                 affiliation= row['affiliation']
+#                 url_pic= row['url_pic']
                 
-                doc= get_author_info(scholarPage)
-                print("Analyzing "+firstName+" "+lastName);
+#                 doc= get_author_info(scholarPage)
+#                 print("Analyzing "+firstName+" "+lastName);
 
-                citationsAll= doc.find_all("td", class_="gsc_rsb_std")[0].text
-                citations5Y= doc.find_all("td", class_="gsc_rsb_std")[1].text
-                hIndexAll= doc.find_all("td", class_="gsc_rsb_std")[2].text
-                hIndex5Y= doc.find_all("td", class_="gsc_rsb_std")[3].text
-                keywords= doc.find_all("a", class_="gsc_prf_inta gs_ibl")
-                kw=""
-                for a in doc.find_all("a", class_="gsc_prf_inta gs_ibl"):
-                    kw=kw+a.text+";"
-                print("keywords: "+kw)
+#                 citationsAll= doc.find_all("td", class_="gsc_rsb_std")[0].text
+#                 citations5Y= doc.find_all("td", class_="gsc_rsb_std")[1].text
+#                 hIndexAll= doc.find_all("td", class_="gsc_rsb_std")[2].text
+#                 hIndex5Y= doc.find_all("td", class_="gsc_rsb_std")[3].text
+#                 keywords= doc.find_all("a", class_="gsc_prf_inta gs_ibl")
+#                 kw=""
+#                 for a in doc.find_all("a", class_="gsc_prf_inta gs_ibl"):
+#                     kw=kw+a.text+";"
+#                 print("keywords: "+kw)
       
-                outWriter.writerow({'First name': firstName, 'Last name': lastName, 'affiliation': affiliation, 'citations': citations, 'scholar_page': scholarPage, 'url_pic': url_pic,
-                    'citationsAll':citationsAll,'citations5Y':citations5Y,'hIndexAll':hIndexAll,'hIndex5Y':hIndex5Y,'keywords':kw})
-                sleep(randint(1,3))
+#                 outWriter.writerow({'First name': firstName, 'Last name': lastName, 'affiliation': affiliation, 'citations': citations, 'scholar_page': scholarPage, 'url_pic': url_pic,
+#                     'citationsAll':citationsAll,'citations5Y':citations5Y,'hIndexAll':hIndexAll,'hIndex5Y':hIndex5Y,'keywords':kw})
+#                 sleep(randint(1,3))
 
